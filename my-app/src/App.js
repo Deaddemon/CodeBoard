b5e79804b8bfc00d0849e2f2f1eefad83f235c0f
vinita poonia <vinitapoonia333@gmail.com>
2022-12-28 02:05:06 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Landing from './components/Landing';
+import Canvas from './whiteboard/canvas';
+
+function App() {
+  return (
+    <div className="App">
+       <Landing/>
+       <hr></hr>
+       <Canvas/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/CodeEditorWindow.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+import Editor from "@monaco-editor/react";
+
+const CodeEditorWindow = ({ onChange, language, code, theme }) => {
+  const [value, setValue] = useState(code || "");
+
+  const handleEditorChange = (value) => {
+     
+    onChange = (action, data) => {
+        switch (action) {
+          case "code": {
+            setValue(data);
+            break;
+          }
+          default: {
+            console.warn("case not handled!", action, data);
+          }
+        }
+      };
+  };
+
+  return (
+    <div className="overlay rounded-md overflow-hidden w-full h-full shadow-4xl">
+      <Editor
+        height="48vh"
+        width={`100%`}
+        language={language || "javascript"}
+        value={value}
+        theme={theme}
+        defaultValue="// some comment"
+        onChange={handleEditorChange}
+      />
+    </div>
+  );
+};
+export default CodeEditorWindow;
