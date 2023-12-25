cef3b2859fa1a6705d66a93a71a8f3686dfb1c86
vinita poonia <vinitapoonia333@gmail.com>
2023-12-25 15:32:23 +0530

    Create a Home.jsx and Home.css

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,25 @@
-
+import React from 'react';
+import { BrowserRouter ,Routes, Route } from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Canvas from './whiteboard/canvas';
+import Home from './home/home';
 
 function App() {
   return (
-    <div className="App">
-       <Landing/>
-       <hr></hr>
-       <Canvas/>
-
-    </div>
+   
+    
+        <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/landing" element={<Landing/>} />
+        <Route path="/canvas" element={<Canvas/>} />
+        </Routes>
+        </BrowserRouter>
+        
+   
+  
+ 
   );
 }
 
